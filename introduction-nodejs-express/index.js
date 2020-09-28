f5f94e6980a553b183651ea9ab244ad8ff5fd797
Mauro Gómez <mauro.f.gmz@gmail.com>
2020-09-28 19:19:20 -0300

    tests were made to consult courses in introduction to graphql

--- introduction-nodejs-express/index.js
+++ introduction-nodejs-express/index.js
@@ -6,20 +6,52 @@ const app = express();
 const express_graphql = require('express-graphql').graphqlHTTP
 const { buildSchema } = require('graphql'); // functionality to write schemas
 
+// data
+const { courses } = require('./data.json');
+
+// GrapqhQL Schema
 const schema = buildSchema(`
     type Query {
-        message: String
+        course(id: Int!): Course
+        courses(topic: String): [Course]
+    }
+
+    type Course {
+        id: Int
+        title: String
+        description: String
+        author: String
+        topic: String
+        url: String
     }
 `);
 
-const root = {
-    message: () => "hello world!"
+let getCourse = (args) => {
+    let id = args.id;
+    return courses.filter(course => {
+        return course.id == id;
+    })[0]
 }
 
+let getCourses = (args) => {
+    if (args.topic) {
+        let topic = args.topic;
+        return courses.filter(course => course.topic === topic);
+    } else {
+        return courses;
+    }
+}
+
+const root = {
+    course: getCourse, 
+    courses: getCourses
+}
+
+// GraphQL endpoint
 app.use('/graphql', express_graphql({
     schema: schema,
-    root: root,
-    graphiql: true
+    rootValue: root,
+    graphiql: true 
 }));
 
 app.listen(3000, () => console.log('server on port 3000'));
