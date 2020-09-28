3ea3531f2f4b7e50e7aa74822132cfa5391a9be6
Mauro Gómez <mauro.f.gmz@gmail.com>
2020-09-28 20:46:35 -0300

    added mutations to intro example

--- introduction-nodejs-express/index.js
+++ introduction-nodejs-express/index.js
@@ -16,6 +16,10 @@ const schema = buildSchema(`
         courses(topic: String): [Course]
     }
 
+    type Mutation {
+        updateCourseTopic(id: Int!, topic: String!): Course
+    }
+
     type Course {
         id: Int
         title: String
@@ -42,9 +46,21 @@ let getCourses = (args) => {
     }
 }
 
+let updateCourseTopic = ({id, topic}) => {
+    courses.map(course => {
+        if (course.id === id) {
+            course.topic = topic;
+            return course;
+        }
+    })
+
+    return courses.filter(course => course.id === id)[0];
+}
+
 const root = {
     course: getCourse, 
-    courses: getCourses
+    courses: getCourses,
+    updateCourseTopic: updateCourseTopic
 }
 
 // GraphQL endpoint
