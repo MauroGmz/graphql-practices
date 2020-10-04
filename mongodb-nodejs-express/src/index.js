899a77d7fb6109ce260bde46a24112be7f711c43
Mauro Gómez <mauro.f.gmz@gmail.com>
2020-10-04 19:49:57 -0300

    added conection to mongodb

--- mongodb-nodejs-express/src/index.js
+++ mongodb-nodejs-express/src/index.js
@@ -1,1 +1,14 @@
 import express from 'express';
+const app = express();
+
+import mongoose from 'mongoose';
+mongoose.connect('mongodb://localhost/graphql-test')
+    .then(() => console.log('mongodb connected'))
+    .catch(err => console.log(err));    
+
+// settings 
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+    console.log('server on port 3000');
+});
