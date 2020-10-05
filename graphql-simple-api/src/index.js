52f66a8ed74d18db61e3b535aef993d0eadc66ce
Mauro Gómez <mauro.f.gmz@gmail.com>
2020-10-05 20:05:31 -0300

    prepared environment to use graphql

--- graphql-simple-api/src/index.js
+++ graphql-simple-api/src/index.js
@@ -1,5 +1,16 @@
-const express = require('express');
+import express from 'express';
+const { graphqlHTTP } = require('express-graphql');
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Hello World"
+    })
+});
+
+app.use('/graphql', graphqlHTTP({
+
+}));
 
 app.listen(3000, () => console.log('Server on port 3000'));
