51f877f5b432ef81cb8c620de85d72e0392142d3
Mauro Gómez <mauro.f.gmz@gmail.com>
2020-10-07 21:34:48 -0300

    finished the simple api, the elimination user by id should be improved

--- graphql-simple-api/src/resolvers.js
+++ graphql-simple-api/src/resolvers.js
@@ -1,5 +1,7 @@
 import { tasks } from "./sample";
 
+import User from "./models/User";
+
 export const resolvers = {
     Query: {
         hello: () => {
@@ -10,6 +12,9 @@ export const resolvers = {
         },
         tasks() {
             return tasks;
+        },
+        async Users() {
+            return await User.find();
         }
     },
     Mutation: {
@@ -17,6 +22,17 @@ export const resolvers = {
             input._id = tasks.length;
             tasks.push(input);
             return input;
+        },
+        async createUser(_, { input }) {
+            const newUser = new User(input);
+            await newUser.save();
+            return newUser;
+        },
+        async deleteUser(_, { _id }) {
+            return await User.findByIdAndDelete(_id);
+        },
+        async updateUser(_, { _id, input }) {
+            return await User.findByIdAndUpdate(_id, input, { new: true });
         }
     }
 };
